e1ca3380a12ac4045ed6b4ce166fd77521bded87
Max Hallinan <maxhallinan@riseup.net>
2019-09-16 11:03:59 -0400

    Add MonadError and MonadThrow instances to the AppT type.

--- src/App.purs
+++ src/App.purs
@@ -1,6 +1,8 @@
 module App 
   ( module App.Env
+  , AppT(..)
   , App
+  , AppErr(..)
   , runApp
   ) where
 
@@ -8,12 +10,25 @@ import Prelude
 
 import App.Env (Env)
 import Effect.Aff (Aff)
+import Control.Monad.Except.Trans (ExceptT, runExceptT)
+import Control.Monad.Error.Class (class MonadError, class MonadThrow)
 import Control.Monad.Reader.Trans (ReaderT, runReaderT)
+import Data.Either (Either)
+import Data.Newtype (class Newtype)
 
-newtype App a = App (ReaderT Env Aff a)
-derive newtype instance functorApp :: Functor App
-derive newtype instance applyApp :: Apply App
-derive newtype instance applicativeApp :: Applicative App
+type App a = AppT AppErr Aff a
 
-runApp :: forall a. Env -> App a -> Aff a
-runApp env (App r) = runReaderT r $ env
+data AppErr = NotFound
+
+newtype AppT e m a = AppT (ExceptT e (ReaderT Env m) a)
+derive newtype instance functorAppT :: Functor m => Functor (AppT e m)
+derive newtype instance applyAppT :: Monad m => Apply (AppT e m)
+derive newtype instance applicativeAppT :: Monad m => Applicative (AppT e m)
+derive newtype instance monadErrorAppT :: Monad m => MonadError e (AppT e m)
+derive newtype instance monadThrowAppT :: Monad m => MonadThrow e (AppT e m)
+
+runApp :: forall a. Env -> App a -> Aff (Either AppErr a)
+runApp = runAppT
+
+runAppT :: forall e m a. Env -> AppT e m a -> m (Either e a)
+runAppT env (AppT e) = runReaderT (runExceptT e) env
--- src/Handler/HelloWorld.purs
+++ src/Handler/HelloWorld.purs
@@ -2,32 +2,28 @@ module Handler.HelloWorld (HelloWorldReq, HelloWorldRes, getHelloWorld) where
 
 import Prelude
 
-import App (App, Env, runApp)
+import App (App, AppErr(..), Env, runApp)
+import Data.Either (Either(..))
 import Data.Tuple (Tuple(..))
 import Effect.Aff (Aff)
 import Server (class Input, class Output)
 
-getHelloWorld :: Env -> (HelloWorldReq -> Aff HelloWorldRes)
-getHelloWorld = makeHandler _getHelloWorld
-
-_getHelloWorld :: HelloWorldReq -> App HelloWorldRes
-_getHelloWorld _ = pure (HelloWorldRes unit)
+getHelloWorld :: Env -> HelloWorldReq -> Aff HelloWorldRes
+getHelloWorld env = map HelloWorldRes <<< runApp env <<< handler
+  
+handler :: HelloWorldReq -> App String
+handler _ = pure "Hello world!"
 
 newtype HelloWorldReq = HelloWorldReq Unit
 
 instance inputHelloWorld :: Input HelloWorldReq where
   toInput _ = HelloWorldReq unit
 
-newtype HelloWorldRes = HelloWorldRes Unit
+newtype HelloWorldRes = HelloWorldRes (Either AppErr String)
 
 instance outputHelloWorld :: Output HelloWorldRes where
-  fromOutput _ = { body: "Hello world!", headers: [Tuple "foo" "bar"], status: 200 }
-
-makeHandler 
-  :: forall i o
-   . Input i 
-  => Output o 
-  => (i -> App o) 
-  -> Env 
-  -> (i -> Aff o)
-makeHandler handler env = runApp env <<< handler
+  fromOutput (HelloWorldRes res) = case res of
+    Left NotFound ->
+      { body: "Not found!", headers: [], status: 404  }
+    Right msg ->
+      { body: msg, headers: [Tuple "foo" "bar"], status: 200  }
