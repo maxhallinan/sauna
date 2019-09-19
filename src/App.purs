8e6270433bb6d345c8e484c63d15059b298739ec
Max Hallinan <maxhallinan@riseup.net>
2019-09-18 22:48:51 -0400

    Parameterize AppT on environment and error types.

--- src/App.purs
+++ src/App.purs
@@ -17,23 +17,23 @@ import Effect.Class (class MonadEffect)
 import Effect.Aff (Aff)
 import Effect.Aff.Class (class MonadAff)
 
-type App a = AppT Aff a
+type App a = AppT Err Env Aff a
 
-newtype AppT m a = AppT (ExceptT Err (ReaderT Env m) a)
-derive newtype instance functorAppT :: Functor m => Functor (AppT m)
-derive newtype instance applyAppT :: Monad m => Apply (AppT m)
-derive newtype instance applicativeAppT :: Monad m => Applicative (AppT m)
-derive newtype instance bindAppT :: Monad m => Bind (AppT m)
-derive newtype instance monadAppT :: Monad m => Monad (AppT m)
-derive newtype instance monadErrorAppT :: Monad m => MonadError Err (AppT m)
-derive newtype instance monadThrowAppT :: Monad m => MonadThrow Err (AppT m)
-derive newtype instance monadAskAppT :: Monad m => MonadAsk Env (AppT m)
-derive newtype instance monadReaderAppT :: Monad m => MonadReader Env (AppT m)
-derive newtype instance monadEffectAppT :: MonadEffect m => MonadEffect (AppT m)
-derive newtype instance monadAffAppT :: MonadAff m => MonadAff (AppT m)
+newtype AppT err env m a = AppT (ExceptT err (ReaderT env m) a)
+derive newtype instance functorAppT :: Functor m => Functor (AppT err env m)
+derive newtype instance applyAppT :: Monad m => Apply (AppT err env m)
+derive newtype instance applicativeAppT :: Monad m => Applicative (AppT err env m)
+derive newtype instance bindAppT :: Monad m => Bind (AppT err env m)
+derive newtype instance monadAppT :: Monad m => Monad (AppT err env m)
+derive newtype instance monadErrorAppT :: Monad m => MonadError err (AppT err env m)
+derive newtype instance monadThrowAppT :: Monad m => MonadThrow err (AppT err env m)
+derive newtype instance monadAskAppT :: Monad m => MonadAsk env (AppT err env m)
+derive newtype instance monadReaderAppT :: Monad m => MonadReader env (AppT err env m)
+derive newtype instance monadEffectAppT :: MonadEffect m => MonadEffect (AppT err env m)
+derive newtype instance monadAffAppT :: MonadAff m => MonadAff (AppT err env m)
 
 runApp :: forall a. Env -> App a -> Aff (Either Err a)
 runApp = runAppT
 
-runAppT :: forall m a. Env -> AppT m a -> m (Either Err a)
+runAppT :: forall err env m a. env -> AppT err env m a -> m (Either err a)
 runAppT env (AppT e) = runReaderT (runExceptT e) env
