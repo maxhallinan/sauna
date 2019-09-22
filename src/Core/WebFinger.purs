a0b47d6154b87d91883106b038646d1ec9194b98
Max Hallinan <maxhallinan@riseup.net>
2019-09-22 13:34:18 -0400

    Add `links` and `aliases` fields to WebFinger.

--- src/Core/WebFinger.purs
+++ src/Core/WebFinger.purs
@@ -1,8 +1,14 @@
-module Core.WebFinger (WebFinger(..), WebFingerFields, toJsonString) where
+module Core.WebFinger
+  ( Link(..)
+  , LinkType(..)
+  , WebFinger(..)
+  , WebFingerFields
+  , toJsonString
+  ) where
 
 import Prelude
 
-import Data.Argonaut (class EncodeJson, encodeJson)
+import Data.Argonaut (class EncodeJson, Json, encodeJson)
 import Data.Argonaut as A
 import Data.Tuple (Tuple(..))
 import Foreign.Object as O
@@ -10,13 +16,41 @@ import Foreign.Object as O
 newtype WebFinger = WebFinger WebFingerFields
 
 type WebFingerFields =
-  { subject :: String
+  { aliases :: Array String
+  , links :: Array Link
+  , subject :: String
   }
 
 instance encodeJsonWebFinger :: EncodeJson WebFinger where
-  encodeJson (WebFinger fields) = A.fromObject obj
-    where obj = O.fromFoldable [ Tuple "subject" (A.fromString fields.subject)
-                               ]
+  encodeJson = encodeWebFinger
+
+encodeWebFinger :: WebFinger -> Json
+encodeWebFinger (WebFinger wf) = A.fromObject $ O.fromFoldable $
+  [ Tuple "aliases" $ A.fromArray (map A.fromString wf.aliases)
+  , Tuple "links" $ A.fromArray (map encodeLink wf.links)
+  , Tuple "subject" $ A.fromString wf.subject
+  ]
+
+newtype Link = Link
+  { href :: String
+  , rel :: String
+  , type_ :: LinkType
+  }
+
+instance encodeJsonLink :: EncodeJson Link where
+  encodeJson = encodeLink
+
+encodeLink :: Link -> Json
+encodeLink (Link link) = A.fromObject $ O.fromFoldable
+  [ Tuple "href" $ A.fromString link.href
+  , Tuple "rel" $ A.fromString link.rel
+  , Tuple "type" $ A.fromString (toMediaType link.type_)
+  ]
+
+data LinkType = ActivityJson
+
+toMediaType :: LinkType -> String
+toMediaType ActivityJson = "application/activity+json"
 
 toJsonString :: WebFinger -> String
 toJsonString = A.stringify <<< encodeJson
--- src/Handler/WebFinger/Get.purs
+++ src/Handler/WebFinger/Get.purs
@@ -32,12 +32,30 @@ toParams req = do
 
 readResource :: Foreign -> Except Err String
 readResource = withExcept toInvalidErr <<< read
-  where read = F.readString <=< F.I.readProp "resource" 
+  where read = F.readString <=< F.I.readProp "resource"
         toInvalidErr = const $ Err.badRequest "Missing `resource` query parameter."
 
 handleGet :: Env -> Request -> Aff Response
 handleGet env = runJsonHandler env 200 handler
 
+{-
+  200 OK
+  Access-Control-Allow-Origin: *
+  Content-Type: application/jrd+json; charset=utf-8
+  {
+    "subject": "acct:foo@host.com",
+    "aliases": [
+      "host.com/users/foo"
+    ]
+    "links": [
+      {
+        "rel": "self",
+        "type": "application/activity+json",
+        "href": "foo.com/users/foo"
+      }
+    ],
+  }
+-}
 handler
   :: forall env m
    . Has DBConnection env
@@ -47,6 +65,9 @@ handler
   => MonadThrow Err m
   => Params
   -> m WebFinger
-handler (Params { username }) = do 
+handler (Params { username }) = do
   Account account <- getAccountByUsername username
-  pure $ WebFinger { subject: account.username }
+  pure $ WebFinger { subject: account.username
+                   , links: []
+                   , aliases: []
+                   }
